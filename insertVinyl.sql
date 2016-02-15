0a7318d0dda44de169063722a524ca63b3e2557a
Caitlin <ctlnwtkns@gmail.com>
2016-02-14 18:59:09 -0800

    employee and partial salesDetail

--- insertVinyl.sql
+++ insertVinyl.sql
@@ -97,3 +97,24 @@ VALUES
 (DEFAULT, 'Simpson', 'Homer', '742 Evergreen Ter', 'Springfield', 'OR', '97477''541-123-4567', 'chunkylover53@aol.com'),
 (DEFAULT, 'Simpson', 'Lisa', '742 Evergreen Ter', 'Springfield', 'OR', '97477', '541-123-7654', 'smartgirl63@yahoo.com'),
 (DEFAULT, 'Simpson', 'Bart', '742 Evergreen Ter', 'Springfield', 'OR', '97477', '541-123-9999', 'bart.simpson@hotmail.com')
+
+--Employee test data
+INSERT INTO Employee (Employeekey, EmployeeNameLast, EmployeeNameFirst)
+VALUES
+(DEFAULT, 'di Prima', 'Diane'),
+(DEFAULT, 'Joplin', 'Janis'),
+(DEFAULT, 'Garcia', 'Jerry')
+
+--Sales test data
+INSERT INTO SaleDetail (salesKey, albumKey, salesQty, salesTax, salesPrice, posKey)
+VALUES 
+(DEFAULT, 2, 1, 1.42, 14.99, ),
+(DEFAULT, 4, 1, , 1.61, 16.99 ),
+(DEFAULT, 5, 2, 1.42, 14.99, ),
+(DEFAULT, 8, 1, 2.28, 24.00, ),
+(DEFAULT, 7, 1, 1.90, 19.99, ),
+(DEFAULT, 9, 2, , , ),
+(DEFAULT, 6, 1, , , ),
+(DEFAULT, 3, 1, , , ),
+(DEFAULT, 1, 1, , , ),
+(DEFAULT, 10, 1 , , , )
