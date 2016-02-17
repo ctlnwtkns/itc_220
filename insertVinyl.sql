5b061930ce9316643163460f6723e864929467fd
Caitlin <ctlnwtkns@gmail.com>
2016-02-17 13:27:37 -0800

    remove DEFAULT values, primarykey attributes

--- insertVinyl.sql
+++ insertVinyl.sql
@@ -13,105 +13,104 @@
 --10. Ibeyi, 'Ibeyi', Downtempo, Soul, R&B, Experimental
 
 --artist test data
-INSERT INTO Artist (artistKey, artistNameLast, artistNameFirst, artistAlias)
+INSERT INTO Artist (artistNameLast, artistNameFirst, artistAlias)
 VALUES 
-(DEFAULT, 'Barnett', 'Courtney', 'NULL'), --1
-(DEFAULT, 'Stevens', 'Sufjan', NULL), --2
-(DEFAULT, 'Smith', 'Gabrielle', 'Eskimeuax'), --3
-(DEFAULT, 'Kalb', 'Oliver', 'Eskimeuax'), --4
-(DEFAULT, 'Walworth', 'Felix', 'Eskimeuax'), --5
-(DEFAULT, 'Greenleaf', 'Jack', 'Eskimeuax'), --6
-(DEFAULT, 'Shelly', 'Joan', NULL), --7
-(DEFAULT, 'Scott', 'Mackenzie', 'Torres'), --8
-(DEFAULT, 'Guðmundsdóttir', 'Björk', 'Björk'), --9
-(DEFAULT, 'Monds-Watson', 'Bridie', 'SOAK'), --10
-(DEFAULT, 'Arnalds', 'Ólafur', NULL), --11
-(DEFAULT, 'Ott', 'Alice Sara', NULL), --12
-(DEFAULT, 'Tucker', 'Cleo', 'Girlpool'), --13
-(DEFAULT, 'Tividad', ' Harmony', 'Girlpool'), --14
-(DEFAULT, 'Diaz', 'Lisa-Kaindé', 'Ibeyi'), --15
-(DEFAULT, 'Diaz', 'Naomi', 'Ibeyi') --16
+('Barnett', 'Courtney', 'NULL'), --1
+('Stevens', 'Sufjan', NULL), --2
+('Smith', 'Gabrielle', 'Eskimeuax'), --3
+('Kalb', 'Oliver', 'Eskimeuax'), --4
+( 'Walworth', 'Felix', 'Eskimeuax'), --5
+( 'Greenleaf', 'Jack', 'Eskimeuax'), --6
+('Shelly', 'Joan', NULL), --7
+( 'Scott', 'Mackenzie', 'Torres'), --8
+( 'Guðmundsdóttir', 'Björk', 'Björk'), --9
+( 'Monds-Watson', 'Bridie', 'SOAK'), --10
+( 'Arnalds', 'Ólafur', NULL), --11
+( 'Ott', 'Alice Sara', NULL), --12
+( 'Tucker', 'Cleo', 'Girlpool'), --13
+( 'Tividad', ' Harmony', 'Girlpool'), --14
+( 'Diaz', 'Lisa-Kaindé', 'Ibeyi'), --15
+( 'Diaz', 'Naomi', 'Ibeyi') --16
 
 --genre look-up
-INSERT INTO Genre (genreKey, genreName)
+INSERT INTO Genre (genreName)
 VALUES
-(DEFAULT, 'Indie Rock'), --1
-(DEFAULT, 'Indie Folk'), --2
-(DEFAULT, 'Ethereal Pop'), --3
-(DEFAULT, 'Traditional Folk'), --4
-(DEFAULT, 'Rock'), --5
-(DEFAULT, 'Electronic'), --6
-(DEFAULT, 'Avant Garde'), --7
-(DEFAULT, 'Classical Crossover'), --8
-(DEFAULT, 'Ambient'), --9
-(DEFAULT, 'Dream Pop'), --10
-(DEFAULT, 'Classical'), --11
-(DEFAULT, 'Folk Punk'), --12
-(DEFAULT, 'Downtempo'), --13
-(DEFAULT, 'Soul'), --14
-(DEFAULT, 'R&B'), --15
-(DEFAULT), 'Experimental') --16
+( 'Indie Rock'), --1
+('Indie Folk'), --2
+('Ethereal Pop'), --3
+( 'Traditional Folk'), --4
+( 'Rock'), --5
+( 'Electronic'), --6
+( 'Avant Garde'), --7
+( 'Classical Crossover'), --8
+( 'Ambient'), --9
+( 'Dream Pop'), --10
+( 'Classical'), --11
+( 'Folk Punk'), --12
+( 'Downtempo'), --13
+( 'Soul'), --14
+( 'R&B'), --15
+( 'Experimental') --16
 
 --speed look-up
 --https://rateyourmusic.com/wiki/Music:Release+attributes#Speed
-INSERT INTO Speed (speedKey, speed)
+INSERT INTO Speed (speed)
 VALUES
-(DEFAULT, '16 rpm'), --1
-(DEFAULT, '33 rpm'), --2
-(DEFAULT, '45 rpm'), --3
-(DEFAULT, '78 rpm'), --4
-(DEFAULT, '80 rpm'), --5
-(DEFAULT, '3¾ IPS'), --6
-(DEFAULT, '7½ IPS') --7
+('16 rpm'), --1
+( '33 rpm'), --2
+( '45 rpm'), --3
+( '78 rpm'), --4
+( '80 rpm'), --5
+( '3¾ IPS'), --6
+( '7½ IPS') --7
 
 --size look-up
 --http://xvinylx.com/wiki/Record_size
-INSERT INTO Size (sizeKey, size)
+INSERT INTO Size (size)
 VALUES
-(DEFAULT, '7 inch'), --1
-(DEFAULT, '10 inch'), --2
-(DEFAULT, '12 inch') --3
+('7 inch'), --1
+('10 inch'), --2
+( '12 inch') --3
 
 --quality look-up
 --https://rateyourmusic.com/wiki/Music:Release+attributes#Quality
-INSERT INTO Quality (qualityKey, weight)
+INSERT INTO Quality (weight)
 VALUES
-(DEFAULT, '120 gram'), --1
-(DEFAULT, '140 gram'), --2
-(DEFAULT, '150 gram'), --3
-(DEFAULT,' 160 gram'), --4
-(DEFAULT, '180 gram'), --5
-(DEFAULT, '200 gram'), --6
-(DEFAULT, '220 gram')  --7
+('120 gram'), --1
+('140 gram'), --2
+('150 gram'), --3
+('160 gram'), --4
+('180 gram'), --5
+('200 gram'), --6
+('220 gram')  --7
 
 --album test data
-INSERT INTO Album (albumKey, albumName, catalogNo, storeItemNo, speedKey, sizeKey)
+INSERT INTO Album (albumName, catalogNo, storeItemNo, speedKey, sizeKey)
 VALUES 
-(DEFAULT, 'Sometimes I Sit and Think, And Sometimes I Just Sit', ), --1
-(DEFAULT, 'Carrie & Lowell', ), --2
-(DEFAULT, 'O.K.', ), --3
-(DEFAULT, 'Over And Even', ), --4
-(DEFAULT, 'Sprinter', ), --5
-(DEFAULT, 'Vulnicura', ), --6
-(DEFAULT, 'Before We Forgot How To Dream', ), --7
-(DEFAULT, 'The Chopin Project', ), --8
-(DEFAULT, 'Before The World Was Big', ), --9
-(DEFAULT, 'Ibeyi', ) --10
+( 'Sometimes I Sit and Think, And Sometimes I Just Sit', ), --1
+( 'Carrie & Lowell', ), --2
+( 'O.K.', ), --3
+( 'Over And Even', ), --4
+( 'Sprinter', ), --5
+( 'Vulnicura', ), --6
+( 'Before We Forgot How To Dream', ), --7
+( 'The Chopin Project', ), --8
+( 'Before The World Was Big', ), --9
+( 'Ibeyi', ) --10
 
---releaseDate test data
-INSERT INTO releaseDate (releaseDateKey, releaseDate)
+INSERT INTO releaseDate (releaseDate)
 VALUES
-(DEFALUT, 2006), --1
-(DEFAUT,  2007), --2
-(DEFAULT, 2008), --3
-(DEFAULT, 2009), --4
-(DEFAULT, 2010), --5 
-(DEFAULT, 2011), --6
-(DEFAULT, 2012), --7
-(DEFAULT, 2013), --8
-(DEFAULT, 2014), --9
-(DEFAULT, 2015), --9
-(DEFAULT, 2016)  --10
+('2006'), --1
+('2007'), --2
+('2008'), --3
+('2009'), --4
+('2010'), --5 
+('2011'), --6
+('2012'), --7
+('2013'), --8
+('2014'), --9
+('2015'), --9
+('2016')  --10
 
 
 --artistAlbum
@@ -133,6 +132,10 @@ VALUES
 (14, 9),
 (15, 10),
 (16, 10)
+
+--AlbumQuality
+--CoverQuality
+--CoverYear
 
 --artistGenre test data
 INSERT INTO artistGenre (artistKey, genreKey)
@@ -170,52 +173,52 @@ VALUES
 (16, 16)
 
 --Customer test data
-INSERT INTO Customer (CustomerKey,customerLastName, customerFirstName, customerAddress, customerCity, customerState, customerZip, customerPhone, customerEmail) 
+INSERT INTO Customer (customerLastName, customerFirstName, customerAddress, customerCity, customerState, customerZip, customerPhone, customerEmail) 
 VALUES 
-(DEFAULT, 'Doe', 'Jane', '123 Fake St', 'Tampa', 'FL', '33617', '813-998-2199', 'janed@gmail.com', --1
-(DEFAULT, 'Doe', 'Joe', '123 Fake St', 'Tampa', 'FL', '33617', '813-998-2101', 'joedoe@gmail.com'), --2
-(DEFAULT, 'Brady', 'Marsha', '12568 Real St', 'Seattle', 'WA', '98101', '206-397-1000', 'marshamarshamarsha@gmail.com'), --3
-(DEFAULT, 'Brady', 'Bobby', '12568 Real St', 'Seattle', 'WA', '98101', '206-397-1001', 'bobbyb@gmail.com'), --4
-(DEFAULT, 'Brady', 'Greg', '12568 Real St', 'Seattle', 'WA', '98101', '206-397-1002', 'bradybunchrulz@gmail.com'), --5
-(DEFAULT, 'Brady', 'Tiger', '12568 Real St', 'Seattle', 'WA', '98101', '206-397-1003', 'tigerthedog@gmail.com'), --6
-(DEFAULT, 'Connor', 'Roseanne', '714 Delaware St', 'Landford', 'IL', '60, '773-978-6669', 'yor@gmail.com'), --7
-(DEFAULT, 'Connor', 'Darlene', '714 Delaware St', 'Landford', 'IL', '60290', '773-918-2169', 'saragilbert@gmail.com'), --8
-(DEFAULT, 'Connor', 'Becky', '714 Delaware St', 'Landford', 'IL', '60290', '813-998-1239', 'whichbeckyami@gmail.com'), --9
-(DEFAULT, 'Simpson', 'Homer', '742 Evergreen Ter', 'Springfield', 'OR', '97477''541-123-4567', 'chunkylover53@aol.com'), --10
-(DEFAULT, 'Simpson', 'Lisa', '742 Evergreen Ter', 'Springfield', 'OR', '97477', '541-123-7654', 'smartgirl63@yahoo.com'), --11
-(DEFAULT, 'Simpson', 'Bart', '742 Evergreen Ter', 'Springfield', 'OR', '97477', '541-123-9999', 'bart.simpson@hotmail.com') --12
+('Doe', 'Jane', '123 Fake St', 'Tampa', 'FL', '33617', '813-998-2199', 'janed@gmail.com'), --1
+('Doe', 'Joe', '123 Fake St', 'Tampa', 'FL', '33617', '813-998-2101', 'joedoe@gmail.com'), --2
+('Brady', 'Marsha', '12568 Real St', 'Seattle', 'WA', '98101', '206-397-1000', 'marshamarshamarsha@gmail.com'), --3
+('Brady', 'Bobby', '12568 Real St', 'Seattle', 'WA', '98101', '206-397-1001', 'bobbyb@gmail.com'), --4
+('Brady', 'Greg', '12568 Real St', 'Seattle', 'WA', '98101', '206-397-1002', 'bradybunchrulz@gmail.com'), --5
+('Brady', 'Tiger', '12568 Real St', 'Seattle', 'WA', '98101', '206-397-1003', 'tigerthedog@gmail.com'), --6
+('Connor', 'Roseanne', '714 Delaware St', 'Landford', 'IL', '60290', '773-978-6669', 'yor@gmail.com'), --7
+('Connor', 'Darlene', '714 Delaware St', 'Landford', 'IL', '60290', '773-918-2169', 'saragilbert@gmail.com'), --8
+('Connor', 'Becky', '714 Delaware St', 'Landford', 'IL', '60290', '813-998-1239', 'whichbeckyami@gmail.com'), --9
+('Simpson', 'Homer', '742 Evergreen Ter', 'Springfield', 'OR', '97477','541-123-4567', 'chunkylover53@aol.com'), --10
+('Simpson', 'Lisa', '742 Evergreen Ter', 'Springfield', 'OR', '97477', '541-123-7654', 'smartgirl63@yahoo.com'), --11
+('Simpson', 'Bart', '742 Evergreen Ter', 'Springfield', 'OR', '97477', '541-123-9999', 'bart.simpson@hotmail.com') --12
 
 --Employee test data
-INSERT INTO Employee (Employeekey, EmployeeNameLast, EmployeeNameFirst)
+INSERT INTO Employee (EmployeeNameLast, EmployeeNameFirst)
 VALUES
-(DEFAULT, 'di Prima', 'Diane'), --1
-(DEFAULT, 'Joplin', 'Janis'), --2
-(DEFAULT, 'Garcia', 'Jerry') --3
+('di Prima', 'Diane'), --1
+('Joplin', 'Janis'), --2
+('Garcia', 'Jerry') --3
 
 --POS test data
-INSERT INTO PointOfSale (posKey, posLocation, posDateTime, posID, customerKey, employeeKey)
+INSERT INTO PointOfSale (posLocation, posDateTime, posID, customerKey, employeeKey)
 VALUES
-(DEFAULT, 'SEAcpR1', 2016-02-14 14:23:56, 'AeFAdFXOkl', 1, 3), --1
-(DEFAULT, 'SEAcpR2', 2016-02-14 14:32:01, 'vKvKqi4uyW', 2, 2), --2
-(DEFAULT, 'SEAcpR2', 2016-02-14 15:14:13, 'si5v2Kz9LD', 3, 2), --3
-(DEFAULT, 'SEAcpR1', 2016-02-14 16:00:14, 'AWCfAU6BuE', 4, 3), --4
-(DEFAULT, 'SEAcpR3', 2016-02-14 16:37:22, 'TtGiEQEoTv', 5, 1), --5
-(DEFAULT, 'SEAcpR3', 2016-02-15 09:45:33, 'z2HrP7e9z5', 6, 1), --6 
-(DEFAULT, 'SEAcpR3', 2016-02-15 12:02:48, 'lJPDcqt0cD', 7, 1), --7
-(DEFAULT, 'SEAcpR1', 2016-02-15 12:12:41, 'kbE3JgnNIl', 8, 2), --8
-(DEFAULT, 'SEAcpR1', 2016-02-15 13:15:14, 'uUKb9vW57p', 9, 2), --9
-(DEFAULT, 'SEAcpR2', 2016-02-15 13:33:53, 'pjNpq3n10F', 10, 3) --10
+( 'SEAcpR1', '2016-02-14 14:23:56', 'AeFAdFXOkl', 1, 3), --1
+( 'SEAcpR2', '2016-02-14 14:32:01', 'vKvKqi4uyW', 2, 2), --2
+( 'SEAcpR2', '2016-02-14 15:14:13', 'si5v2Kz9LD', 3, 2), --3
+( 'SEAcpR1', '2016-02-14 16:00:14', 'AWCfAU6BuE', 4, 3), --4
+( 'SEAcpR3', '2016-02-14 16:37:22', 'TtGiEQEoTv', 5, 1), --5
+( 'SEAcpR3', '2016-02-15 09:45:33', 'z2HrP7e9z5', 6, 1), --6 
+( 'SEAcpR3', '2016-02-15 12:02:48', 'lJPDcqt0cD', 7, 1), --7
+( 'SEAcpR1', '2016-02-15 12:12:41', 'kbE3JgnNIl', 8, 2), --8
+( 'SEAcpR1', '2016-02-15 13:15:14', 'uUKb9vW57p', 9, 2), --9
+( 'SEAcpR2', '2016-02-15 13:33:53', 'pjNpq3n10F', 10, 3) --10
 
 --Sales test data
-INSERT INTO SaleDetail (salesKey, albumKey, salesQty, salesTax, salesPrice, posKey)
+INSERT INTO SaleDetail (albumKey, salesQty, salesTax, salesPrice, posKey)
 VALUES 
-(DEFAULT, 2, 1, 1.42, 14.99, 1), --1
-(DEFAULT, 4, 1, 1.61, 16.99, 2), --2
-(DEFAULT, 5, 2, 1.42, 14.99, 3), --3
-(DEFAULT, 8, 1, 2.28, 24.00, 4), --4
-(DEFAULT, 7, 1, 1.90, 19.99, 5), --5
-(DEFAULT, 9, 2, 1.90, 19.98, 6), --6
-(DEFAULT, 6, 1, 1.75, 18.41, 7), --7
-(DEFAULT, 3, 1, 1.95, 20.53, 8), --8
-(DEFAULT, 1, 1, 1.61, 16.99, 9), --9
-(DEFAULT, 10,1, 1.90, 19.99, 10) --10
+( 2, 1, 1.42, 14.99, 1), --1
+( 4, 1, 1.61, 16.99, 2), --2
+( 5, 2, 1.42, 14.99, 3), --3
+( 8, 1, 2.28, 24.00, 4), --4
+( 7, 1, 1.90, 19.99, 5), --5
+( 9, 2, 1.90, 19.98, 6), --6
+( 6, 1, 1.75, 18.41, 7), --7
+( 3, 1, 1.95, 20.53, 8), --8
+( 1, 1, 1.61, 16.99, 9), --9
+(10,1, 1.90, 19.99, 10) --10
