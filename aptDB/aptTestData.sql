cc0b27f0cd5b4b342cf5c09679c45a1b87f827d3
Caitlin <ctlnwtkns@gmail.com>
2016-03-09 13:13:02 -0800

    Update aptTestData.sql

--- aptDB/aptTestData.sql
+++ aptDB/aptTestData.sql
@@ -19,22 +19,22 @@ VALUES  (101),
 
 
 --TenantLease
-INSERT INTO TenantLease (tenantLeaseKey, aptKey, tenantKey, leaseKey) 
-VALUES  (DEFAULT, 1, 1, 1),
-        (DEFAULT, 2, 2, 2),
-        (DEFAULT, 3, 3, 3),
-        (DEFAULT, 4, 4, 4),
-        (DEFAULT, 5, 5, 5),
-        (DEFAULT, 6, 6, 6),
-        (DEFAULT, 7, 7, 7),
-        (DEFAULT, 8, 8, 8),
-        (DEFAULT, 9, 9, 9),
-        (DEFAULT, 10, 10, 10),
-        (DEFAULT, 11, 11, 11),
-        (DEFAULT, 12, 12, 12),
-        (DEFAULT, 13, 13, 13),
-        (DEFAULT, 14, 14, 14),
-        (DEFAULT, 15, 15, 15)
+INSERT INTO TenantLease (aptKey, tenantKey, leaseKey) 
+VALUES  (1, 1, 1),
+        (2, 2, 2),
+        (3, 3, 3),
+        (4, 4, 4),
+        ( 5, 5, 5),
+        (6, 6, 6),
+        ( 7, 7, 7),
+        (8, 8, 8),
+        ( 9, 9, 9),
+        (10, 10, 10),
+        (11, 11, 11),
+        (12, 12, 12),
+        ( 13, 13, 13),
+        (14, 14, 14),
+        (15, 15, 15)
 
 --        Query to get rentDateDue from Lease
 --        SELECT (DATEPART(YYYY-MM, CURDATE()) + '-' + DATEPART(DD, leaseDateStart)) AS rentDateDue --the same day of whatever current month as the least start date
@@ -43,8 +43,8 @@ VALUES  (DEFAULT, 1, 1, 1),
 --        AND tenantLeaseKey = (SELECT tenantLeaseKey FROM tenantLease a WHERE a.leaseKey = b.leaseKey) --for whatever lease matches the tenantLeaseKey stored as current value
 
 --RentPaid
-INSERT INTO RentPaid (rentPaidKey, tenantLeaseKey, rentDateDue, rentDateReceived) 
-VALUES  ( DEFAULT, 
+INSERT INTO RentPaid (tenantLeaseKey, rentDateDue, rentDateReceived) 
+VALUES  (  
           ( SELECT (DATEPART(YYYY-MM, CURDATE()) + '-' + DATEPART(DD, leaseDateStart)) AS rentDateDue --the same day of whatever current month as the least start date
             FROM Lease b
             WHERE CURDATE() BETWEEN leaseDateStart AND leaseDateEnd
@@ -56,7 +56,7 @@ VALUES  ( DEFAULT,
           DATEPART(YYYY-MM-DD, GETDATE()), 
           12
         ),
-        ( DEFAULT, 
+        (  
           ( SELECT (DATEPART(YYYY-MM, CURDATE()) + '-' + DATEPART(DD, leaseDateStart)) AS rentDateDue --the same day of whatever current month as the least start date
             FROM Lease b
             WHERE CURDATE() BETWEEN leaseDateStart AND leaseDateEnd
@@ -68,7 +68,7 @@ VALUES  ( DEFAULT,
           DATEPART(YYYY-MM-DD, GETDATE()), 
           2
         ),
-        ( DEFAULT, 
+        (  
           ( SELECT (DATEPART(YYYY-MM, CURDATE()) + '-' + DATEPART(DD, leaseDateStart)) AS rentDateDue --the same day of whatever current month as the least start date
             FROM Lease b
             WHERE CURDATE() BETWEEN leaseDateStart AND leaseDateEnd
@@ -80,7 +80,7 @@ VALUES  ( DEFAULT,
           DATEPART(YYYY-MM-DD, GETDATE()), 
           8
         ),
-        ( DEFAULT, 
+        (
           ( SELECT (DATEPART(YYYY-MM, CURDATE()) + '-' + DATEPART(DD, leaseDateStart)) AS rentDateDue --the same day of whatever current month as the least start date
             FROM Lease b
             WHERE CURDATE() BETWEEN leaseDateStart AND leaseDateEnd
@@ -92,7 +92,7 @@ VALUES  ( DEFAULT,
           DATEPART(YYYY-MM-DD, GETDATE()), 
           4
         ),
-        ( DEFAULT, 
+        (
           ( SELECT (DATEPART(YYYY-MM, CURDATE()) + '-' + DATEPART(DD, leaseDateStart)) AS rentDateDue --the same day of whatever current month as the least start date
             FROM Lease b
             WHERE CURDATE() BETWEEN leaseDateStart AND leaseDateEnd
@@ -104,7 +104,7 @@ VALUES  ( DEFAULT,
           DATEPART(YYYY-MM-DD, GETDATE()), 
           7
         ),
-        ( DEFAULT, 
+        (
           ( SELECT (DATEPART(YYYY-MM, CURDATE()) + '-' + DATEPART(DD, leaseDateStart)) AS rentDateDue --the same day of whatever current month as the least start date
             FROM Lease b
             WHERE CURDATE() BETWEEN leaseDateStart AND leaseDateEnd
@@ -116,7 +116,7 @@ VALUES  ( DEFAULT,
           DATEPART(YYYY-MM-DD, GETDATE()), 
           13
         ),
-        ( DEFAULT, 
+        (
           ( SELECT (DATEPART(YYYY-MM, CURDATE()) + '-' + DATEPART(DD, leaseDateStart)) AS rentDateDue --the same day of whatever current month as the least start date
             FROM Lease b
             WHERE CURDATE() BETWEEN leaseDateStart AND leaseDateEnd
@@ -128,7 +128,7 @@ VALUES  ( DEFAULT,
           DATEPART(YYYY-MM-DD, GETDATE()), 
           10
         ),
-        ( DEFAULT, 
+        ( 
           ( SELECT (DATEPART(YYYY-MM, CURDATE()) + '-' + DATEPART(DD, leaseDateStart)) AS rentDateDue --the same day of whatever current month as the least start date
             FROM Lease b
             WHERE CURDATE() BETWEEN leaseDateStart AND leaseDateEnd
@@ -140,7 +140,7 @@ VALUES  ( DEFAULT,
           DATEPART(YYYY-MM-DD, GETDATE()), 
           5
         ),
-        ( DEFAULT, 
+        (
           ( SELECT (DATEPART(YYYY-MM, CURDATE()) + '-' + DATEPART(DD, leaseDateStart)) AS rentDateDue --the same day of whatever current month as the least start date
             FROM Lease b
             WHERE CURDATE() BETWEEN leaseDateStart AND leaseDateEnd
@@ -152,7 +152,7 @@ VALUES  ( DEFAULT,
           DATEPART(YYYY-MM-DD, GETDATE()), 
           15
         ),
-        ( DEFAULT, 
+        ( 
           ( SELECT (DATEPART(YYYY-MM, CURDATE()) + '-' + DATEPART(DD, leaseDateStart)) AS rentDateDue --the same day of whatever current month as the least start date
             FROM Lease b
             WHERE CURDATE() BETWEEN leaseDateStart AND leaseDateEnd
@@ -164,7 +164,7 @@ VALUES  ( DEFAULT,
           DATEPART(YYYY-MM-DD, GETDATE()), 
           6
         ),
-        ( DEFAULT, 
+        ( 
           ( SELECT (DATEPART(YYYY-MM, CURDATE()) + '-' + DATEPART(DD, leaseDateStart)) AS rentDateDue --the same day of whatever current month as the least start date
             FROM Lease b
             WHERE CURDATE() BETWEEN leaseDateStart AND leaseDateEnd
@@ -176,7 +176,7 @@ VALUES  ( DEFAULT,
           DATEPART(YYYY-MM-DD, GETDATE()), 
           3
         ),
-        ( DEFAULT, 
+        ( 
           ( SELECT (DATEPART(YYYY-MM, CURDATE()) + '-' + DATEPART(DD, leaseDateStart)) AS rentDateDue --the same day of whatever current month as the least start date
             FROM Lease b
             WHERE CURDATE() BETWEEN leaseDateStart AND leaseDateEnd
@@ -188,7 +188,7 @@ VALUES  ( DEFAULT,
           DATEPART(YYYY-MM-DD, GETDATE()), 
           7
         ),
-        ( DEFAULT, 
+        ( 
           ( SELECT (DATEPART(YYYY-MM, CURDATE()) + '-' + DATEPART(DD, leaseDateStart)) AS rentDateDue --the same day of whatever current month as the least start date
             FROM Lease b
             WHERE CURDATE() BETWEEN leaseDateStart AND leaseDateEnd
@@ -200,7 +200,7 @@ VALUES  ( DEFAULT,
           DATEPART(YYYY-MM-DD, GETDATE()), 
           14
         ),
-        ( DEFAULT, 
+        ( 
           ( SELECT (DATEPART(YYYY-MM, CURDATE()) + '-' + DATEPART(DD, leaseDateStart)) AS rentDateDue --the same day of whatever current month as the least start date
             FROM Lease b
             WHERE CURDATE() BETWEEN leaseDateStart AND leaseDateEnd
@@ -212,7 +212,7 @@ VALUES  ( DEFAULT,
           DATEPART(YYYY-MM-DD, GETDATE()), 
           11
         ),
-        ( DEFAULT, 
+        (  
           ( SELECT (DATEPART(YYYY-MM, CURDATE()) + '-' + DATEPART(DD, leaseDateStart)) AS rentDateDue --the same day of whatever current month as the least start date
             FROM Lease b
             WHERE CURDATE() BETWEEN leaseDateStart AND leaseDateEnd
@@ -224,7 +224,7 @@ VALUES  ( DEFAULT,
           DATEPART(YYYY-MM-DD, GETDATE()), 
           1
         ),
-        ( DEFAULT, 
+        (  
           ( SELECT (DATEPART(YYYY-MM, CURDATE()) + '-' + DATEPART(DD, leaseDateStart)) AS rentDateDue --the same day of whatever current month as the least start date
             FROM Lease b
             WHERE CURDATE() BETWEEN leaseDateStart AND leaseDateEnd
@@ -239,19 +239,19 @@ VALUES  ( DEFAULT,
 
 
 --AptMaint
-INSERT INTO AptMaint (aptMaintKey, maintCostKey, aptKey) 
-VALUES  (DEFAULT, 1,10),
-        (DEFAULT, 2,12),
-        (DEFAULT, 3,1),
-        (DEFAULT, 4,3),
-        (DEFAULT, 5,2),
-        (DEFAULT, 6,8),
-        (DEFAULT, 7,9),
-        (DEFAULT, 8,13),
-        (DEFAULT, 9,5),
-        (DEFAULT, 10,14),
-        (DEFAULT, 11,4),
-        (DEFAULT, 12,6),
-        (DEFAULT, 13,11),
-        (DEFAULT, 14,7),
-        (DEFAULT, 15,15)
+INSERT INTO AptMaint (maintCostKey, aptKey) 
+VALUES  (1,10),
+        (2,12),
+        (3,1),
+        (4,3),
+        (5,2),
+        (6,8),
+        (7,9),
+        (8,13),
+        (9,5),
+        (10,14),
+        (11,4),
+        (12,6),
+        (13,11),
+        (14,7),
+        (15,15)
